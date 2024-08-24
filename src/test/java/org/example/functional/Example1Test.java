eb861fc3c6238e56b5f2ac8f64256a413bbd552e
shyamradhekumartech <shyamradhekumartech@gmail.com>
2024-08-24 11:05:53 +0530

    Test Added

---
+++ src/test/java/org/example/functional/Example1Test.java
@@ -0,0 +1,28 @@
+package org.example.functional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class Example1Test {
+
+    private Example1 example1;
+
+    @BeforeEach
+    void setUp() {
+        example1 = new Example1();
+    }
+
+    @Test
+    void findEvenNumUsingArray() {
+        // Arrange
+        int[] array = new int[] {1, 2, 3, 4, 5};
+
+        // Act
+        int[] newArray = example1.findEvenNumUsingArray(array);
+
+        // Assert
+        Assertions.assertNotNull(newArray);
+    }
+
+}
